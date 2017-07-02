d1b806c803d364beb4551ab7bb7a7f7338958613
alex <alexgulakov@gmail.com>
2017-07-01 22:40:40 -0700

    .

--- face_detect.py
+++ face_detect.py
@@ -1,40 +1,24 @@
 #sudo apt-get install libopencv-dev python-opencv
+
 import datetime
 import logging
-
+import json
 import cv2
 import os
-from app.utils.constants import ROOT_PATH
 
 
+imagePath = "./faces2.jpg"
 
-class FaceDetect:
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+faces = cv2.CascadeClassifier("./opencv-face.xml").detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=1,
+    minSize=(20, 20),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
 
-    def detect(self, imagePath):
-
-
-        before = datetime.datetime.now()
-
-        # Read the image
-        image = cv2.imread(imagePath)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        faces = cv2.CascadeClassifier(os.path.join(ROOT_PATH, "app/utils/opencv-face.xml")).detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags = cv2.CASCADE_SCALE_IMAGE
-        )
-
-        rez = {
-            "number_faces":  len(faces),
-            "face_boxes": (faces),
-            "time_elapsed": datetime.datetime.now() - before
-        }
-
-
-
-        return rez
-
+print(str(faces))
